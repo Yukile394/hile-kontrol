a767060ba4431343794dbed0d66f79a59262f472
Yukile394 <muhamedmuhet@gmail.com>
2026-06-26 14:04:13 +0300

    Add files via upload

---
+++ src/constants/cheatList.js
@@ -0,0 +1,16 @@
+export const CHEAT_KEYWORDS = [
+  "wurst", "impact", "meteor", "future", "xaero", "killaura", "kill-aura",
+  "autoclicker", "auto-clicker", "bhop", "bunnyhop", "aimbot", "aim-bot",
+  "freecam", "free-cam", "xray", "x-ray", "esp", "wallhack", "wall-hack",
+  "nofall", "no-fall", "flight", "fly-hack", "speed-hack", "scaffold",
+  "baritone", "liquidbounce", "inertia", "aristois", "salhack", "rusherhack",
+  "cheat", "hack", "exploit", "bypass", "injection", "ghostclient",
+  "autototem", "antiknockback", "reach", "criticals", "fastplace", "nuker",
+  "chest-stealer", "autoeat", "velocity", "triggerbot", "fullbright"
+];
+
+export const SUSPICIOUS_REPOS = [
+  "wurst-client", "impact-client", "meteor-client", "future-client",
+  "liquidbounce", "aristois", "salhack", "rusherhack", "ghostclient",
+  "minecraft-hack", "minecraft-cheat", "mc-cheat", "mc-hack"
+];
